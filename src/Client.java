856f2336847b8ea2497241f9875c58e333e8be6f
Giorgos Moustakas <icsd11102@icsd.aegean.gr>
2015-04-28 21:32:02 +0300

    yo

---
+++ src/Client.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author giorgosmous
+ */
+public class Client {
+    public Client() {
+        try {
+            // Ανοίγουμε νέο socket
+            Socket sock = new Socket("127.0.0.1", 6666);
+            
+            // Αποθηκεύουμε τις δύο ροές
+            ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
+            
+            // Αποστέλλουμε την εντολή start για να ξεκινήσει η συναλλαγή
+            out.writeObject("START");
+            
+            // Περιμένουμε μέχρι να λάβουμε ανταπόκριση απο τον server
+            try {
+                System.out.println("Server " + (String) in.readObject());
+            } catch (ClassNotFoundException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
